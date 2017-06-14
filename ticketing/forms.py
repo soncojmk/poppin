d413f365d0e97055c1e15201679905af3a1a86d2
Alex Patin <ajp5755@psu.edu>
2017-06-13 23:47:05 -0400

    Create forms.py

--- ticketing/forms.py
+++ ticketing/forms.py
@@ -4,5 +4,8 @@ class CreditCardForm(forms.Form):
     cvv = forms.IntegerField()
     card_number = forms.IntegerField()
     name = forms.CharField(max_length=40)
-    exp_year = forms.Integer()
-    exp_month = forms.Integer()
+    YEAR_CHOICES = (('17', '17'),('18', '18'),('19','19'),('20','20'),('21','21'),('22','22'))
+    MONTH_CHOICES = (('01','01'),('02','02'),('03','03'),('04','04'),('05','05'),('06','06'),('07','07'),
+                     ('08','08'),('09','09'),('10','10'),('11','11'),('12','12'))
+    exp_year = forms.ChoiceField(choices=YEAR_CHOICES)
+    exp_month = forms.ChoiceField(choices=MONTH_CHOICES)
